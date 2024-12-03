e822c6ee3eb24361bd66f1e72d34d77e6840873c
raeoffrost <raeoffrost@gmail.com>
2024-12-02 23:24:22 -0600

    Added Event Delegation to Gallery

--- app.js
+++ app.js
@@ -1,95 +1,43 @@
 import anime from "./modules/animejs/lib/anime.es.js";
-let isOpen = false;
+
 document.addEventListener('DOMContentLoaded', () => {
-  let targets = document.getElementById('wrapper');
-  let wrapperStyle = wrapper.style;
-  let button = document.getElementById('button');
-  button.addEventListener('click', () => {
-    if (isOpen) {
+  const gallery = document.querySelector('.gallery');
+
+  gallery.addEventListener('click', (event) => {
+    const button = event.target.closest('.button');
+    // dont run animation if not btn
+    if (!button) return;
+
+    const root = button.closest('.root');
+    const wrapper = root.querySelector('.wrapper');
+
+    // Toggle text with animation
+    if (wrapper.classList.contains('open')) {
       anime({
-        targets,
+        targets: wrapper,
         height: 0,
         opacity: [1, 0],
         duration: 400,
         easing: 'easeOutQuad',
         complete() {
-          isOpen = false;
-          wrapperStyle.display = '';
+          wrapper.classList.remove('open');
+          wrapper.style.display = 'none';
         }
       });
     } else {
-      isOpen = true;
-      wrapperStyle.display = 'block';
-      wrapperStyle.height = '0px';
+      wrapper.style.display = 'block';
+      wrapper.style.height = '0px';
+      
       anime({
-        targets,
+        targets: wrapper,
         height: el => el.scrollHeight,
         opacity: [0, 1],
         duration: 400,
-        easing: 'easeOutCubic'
+        easing: 'easeOutCubic',
+        complete() {
+          wrapper.classList.add('open');
+        }
       });
     }
-  }, false);
-}, false);
-document.addEventListener('DOMContentLoaded', () => {
-    let targets = document.getElementById('wrapper2');
-    let wrapperStyle = wrapper2.style;
-    let button = document.getElementById('button2');
-    button.addEventListener('click', () => {
-      if (isOpen) {
-        anime({
-          targets,
-          height: 0,
-          opacity: [1, 0],
-          duration: 400,
-          easing: 'easeOutQuad',
-          complete() {
-            isOpen = false;
-            wrapperStyle.display = '';
-          }
-        });
-      } else {
-        isOpen = true;
-        wrapperStyle.display = 'block';
-        wrapperStyle.height = '0px';
-        anime({
-          targets,
-          height: el => el.scrollHeight,
-          opacity: [0, 1],
-          duration: 400,
-          easing: 'easeOutCubic'
-        });
-      }
-    }, false);
-  }, false);
-  document.addEventListener('DOMContentLoaded', () => {
-    let targets = document.getElementById('wrapper3');
-    let wrapperStyle = wrapper3.style;
-    let button = document.getElementById('button3');
-    button.addEventListener('click', () => {
-      if (isOpen) {
-        anime({
-          targets,
-          height: 0,
-          opacity: [1, 0],
-          duration: 400,
-          easing: 'easeOutQuad',
-          complete() {
-            isOpen = false;
-            wrapperStyle.display = '';
-          }
-        });
-      } else {
-        isOpen = true;
-        wrapperStyle.display = 'block';
-        wrapperStyle.height = '0px';
-        anime({
-          targets,
-          height: el => el.scrollHeight,
-          opacity: [0, 1],
-          duration: 400,
-          easing: 'easeOutCubic'
-        });
-      }
-    }, false);
-  }, false);
+  });
+});
